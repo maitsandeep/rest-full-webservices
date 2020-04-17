14860660e1a182b64a2efefbf1ceb4ee06d685e3
sandeep <maitsandeep@gmail.com>
2020-04-17 14:00:45 +0530

    commit

---
+++ rest-full-webservices/src/main/java/com/rest/webservices/restfullwebservices/RestFullWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rest.webservices.restfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullWebservicesApplication.class, args);
+	}
+
+}
